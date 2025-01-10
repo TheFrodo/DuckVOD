152c2af863c99024288a233c1cd5e852b37e6ddf
TheFrodoDE <Berger-Alexander99@outlook.de>
2025-01-10 19:46:52 +0100

    changed logo
    Change logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const { isLoggedIn } = useAuthStore();
 
   useEffect(() => {
-    document.title = "Ganymede";
+    document.title = "DuckVOD";
   }, []);
 
   return (
